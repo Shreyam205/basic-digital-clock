e1910a5d3b2c6ac9a3cf356787bee6154a101e33
Shreyam205 <140100213+Shreyam205@users.noreply.github.com>
2025-05-22 23:41:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const time = document.getElementById('time')
+const tf = document.getElementById('timeformat')
+
+document.addEventListener('DOMContentLoaded', () => {
+    setInterval(showTime, 1000)
+})
+
+const showTime = () => {
+    let date = new Date()
+
+    let hr = date.getHours()
+    let min = date.getMinutes()
+    let secs = date.getSeconds()
+
+    hr = hr<10 ? `0${hr}` : hr;
+    min = min<10 ? `0${min}` : min;
+    secs = secs<10 ? `0${secs}` : secs;
+
+    time.innerHTML = `${hr}:${min}:${secs}`
+
+    timeformat.innerHTML = hr>12 ? "PM" : "AM"
+
+    // console.log("hours: "+ hr + " mins: "+ min + " secs: "+ secs);
+    
+}
